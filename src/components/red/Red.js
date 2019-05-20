60b5aa32b431b5640341dd74156670807ae248e6
Leslie Shea <leslieyshea@gmail.com>
2019-05-20 11:13:47 -0700

    created red component, displays red splotch

---
+++ src/components/red/Red.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Red() {
+  const style = {
+    backgroundColor: 'red',
+    width: '50vh',
+    height: '50vh'
+  };
+
+  return <div style={style}></div>;
+}
