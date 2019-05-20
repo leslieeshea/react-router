b4eb8bb52b84f82c39bb06e8915f8a434eb82c8c
Leslie Shea <leslieyshea@gmail.com>
2019-05-20 11:16:31 -0700

    created yellow component, displays yellow splotch

---
+++ src/components/yellow/Yellow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Yellow() {
+  const style = {
+    backgroundColor: 'yellow',
+    width: '50vh',
+    height: '50vh'
+  };
+
+  return <div style={style}></div>;
+}
