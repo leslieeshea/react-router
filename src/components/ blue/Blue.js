2c06413a4a20978cbc9b239455b6dbe8066e5edc
Leslie Shea <leslieyshea@gmail.com>
2019-05-20 11:15:06 -0700

    created blue component, displays blue splotch

---
+++ src/components/ blue/Blue.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Blue() {
+  const style = {
+    backgroundColor: 'blue',
+    width: '50vh',
+    height: '50vh'
+  };
+
+  return <div style={style}></div>;
+}
