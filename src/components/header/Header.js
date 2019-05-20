4c0c7e032356af022222e3adb49415e0f0a25413
Leslie Shea <leslieyshea@gmail.com>
2019-05-20 11:40:17 -0700

    added h1 and new color

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header>
+      <h1>Simple Color Routes</h1>
       <nav>
         <p><Link to="/red">Red</Link></p>
         <p><Link to="/blue">Blue</Link></p>
         <p><Link to="/yellow">Yellow</Link></p>
+        <p><Link to="/%239583AB">Opulence Purple Pearl</Link></p>
       </nav>
     </header>
   );
